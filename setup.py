8032131622fb2ca00ed799cf0d2474d99481b60e
BL1Z33 <soulmewo2@gmail.com>
2023-08-16 12:15:27 -0400

    "Added create_package() and a 'long description' to PyPI."

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='a4',
-    version='0.4',
+    version='0.5',
     description='A Python package for terminal utilities and screenshot capturing.',
     author='BL1Z33',
     packages=['a4'],
@@ -12,4 +12,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
     ],
+    long_description=''A4 is a Python package for terminal utilities and screenshot capturing, and more. Made by BL1Z33 with help from ChatGPT, A4 strives to be the best Python package that currently exists on the internet. Download the source code at https://github.com/BL1Z33/a4 and visit the creator's website at https://bl1z33.github.io"
 )
